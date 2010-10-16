1cf4ff33d07dae9a7534efdad90f9b404289d55b
jimmy.beaubien <jimmy.beaubien@8ee7db41-403f-03ee-0b30-02c333d15193>
2010-10-16 05:15:45 +0000

    Added basic factory functionality.

--- src/Game/Entity.h
+++ src/Game/Entity.h
@@ -3,6 +3,8 @@
 
 #include "SigSlot.h"
 
+#include <vector>
+
 class CEngine;
 
 // this class defines a basic entity of the engine.
@@ -18,8 +20,55 @@ public:
 template <class T>
 class CEntityFactory
 {
-	//todo
+	bool Create( T** out_pEntity ) 
+	{
+		if( m_vecFree.size() != 0 )
+		{
+			(*out_pEntity) =  m_vecFree.pop_back();
+			return true;
+		}
+		(*out_pEntity) = 0;
+		return false;
+	}
+	bool Destroy( T** in_pEntity )
+	{
+		if( *in_pEntity != 0 )
+		{
+			m_vecFree.push_back((*in_pEntity));
+			return true;
+		}
+		
+		return false;
+	}
 
+	void Precache( unsigned int in_nCount )
+	{
+		T::Precache();
+
+		m_vecFree.ReservedStream0(in_nCount);
+
+		for( int i=0; i<in_nCount; i++ )
+		{
+			m_vecFree.push_back( new T );
+		}
+	}
+
+	void Uncache()
+	{
+		T::Uncache();
+
+		m_lstFree.begin()
+		
+		while( m_vecFree.size() != 0 ) 
+		{
+			delete m_vecFree.back();
+			m_vecFree.pop_back();
+		}		
+		m_vecFree.clear();
+	}
+
+private:
+	std::vector<T*>	m_vecFree;
 };
 
 #endif//_ENTITY_H
