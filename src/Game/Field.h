233c2e814d9219b551929ffede6244195dde5db0
jimmy.beaubien <jimmy.beaubien@8ee7db41-403f-03ee-0b30-02c333d15193>
2010-10-17 04:01:46 +0000

    Added preliminary field data.

--- src/Game/Field.h
+++ src/Game/Field.h
@@ -2,17 +2,40 @@
 #define _FIELD_H
 
 #include "SigSlot.h"
+#include "../gfx.h"
+
+#define FIELD_SIZE_X	1024
+#define FIELD_SIZE_Y	512
 
 class CEngine;
 
 class CField : public has_slots<>
 {
 public:
+	CField();
+
 	void Update( float in_fDeltaTime );
 	void Render();
+	void LoadFile( const char* in_szFilename );
 
 	virtual void Connect( CEngine* );
 	virtual void Disconnect( CEngine* );
+
+private:
+	void RebuildGrid();
+
+private:
+	typedef struct  
+	{
+		unsigned int nDensity;
+
+	} Cell;
+
+	Cell*	m_pCell;
+
+	// Debug grid
+	Vertex*		vb_grid;
+	int vb_grid_size;
 };
 
 #endif//_FIELD_H
