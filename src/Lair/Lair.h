6090b34377eb8ce32064ceff3be2563b5833d20c
jimmy.beaubien <jimmy.beaubien@8ee7db41-403f-03ee-0b30-02c333d15193>
2010-11-08 05:37:21 +0000

    Initial functional Lair interface.

---
+++ src/Lair/Lair.h
@@ -0,0 +1,35 @@
+#ifndef _LAIR_H
+#define _LAIR_H
+
+#define DEFINE( classname )													\
+public:																		\
+	static classname*	Get##classname() { return Get()->m_p##classname; }	\
+private:																	\
+	classname*	m_p##classname;
+
+#include "Log/Log.h"
+#include "Image/Image.h"
+#include "Texture/Texture.h"
+
+class Lair
+{	
+public:
+	Lair();
+	virtual ~Lair();
+	
+	static void Create()	{ if( ms_pInstance == 0 ) ms_pInstance = new Lair; }
+	static void Release()	{ if( ms_pInstance != 0 ) { delete ms_pInstance; ms_pInstance = 0; } }
+	
+	DEFINE(LogMan);
+	DEFINE(ImageMan);
+	DEFINE(TextureMan);
+	
+private:
+	static Lair* Get() { return ms_pInstance; }
+
+private:
+	static Lair*	ms_pInstance;
+
+};
+
+#endif//_LAIR_H
