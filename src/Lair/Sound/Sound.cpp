5d64037af6cf4002e493d5813845c5d3791a0001
jimmy.beaubien <jimmy.beaubien@8ee7db41-403f-03ee-0b30-02c333d15193>
2010-11-11 03:25:18 +0000

    Added sound map management for loading. Fixed bug related to looping while playback.

--- src/Lair/Sound/Sound.cpp
+++ src/Lair/Sound/Sound.cpp
@@ -3,18 +3,23 @@
 
 #include "../Lair.h"
 
-
 bool Sound::Load( const std::string& in_szFilename, FMOD::System* in_pFmodSystem )
 {
 	m_pFmodSystem = in_pFmodSystem;
-	SoundMan::ErrorCheck( m_pFmodSystem->createStream( in_szFilename.c_str(), FMOD_HARDWARE | FMOD_LOOP_NORMAL | FMOD_2D, 0, &m_pFmodSound ) );
+//	SoundMan::ErrorCheck( m_pFmodSystem->createStream( in_szFilename.c_str(), FMOD_SOFTWARE | FMOD_LOOP_NORMAL | FMOD_2D, 0, &m_pFmodSound ) );
+	SoundMan::ErrorCheck( m_pFmodSystem->createSound( in_szFilename.c_str(), FMOD_SOFTWARE | FMOD_LOOP_NORMAL | FMOD_2D, 0, &m_pFmodSound ) );
 
 	return true;
 }
 
-void Sound::Play( bool in_bLoop )
+void Sound::Play( bool in_bLoop, bool in_bNewChannel )
 {
-	SoundMan::ErrorCheck( m_pFmodSystem->playSound(FMOD_CHANNEL_FREE, m_pFmodSound, false, &m_pFmodChannel ) );
+//	if( in_bNewChannel )
+//		m_pFmodChannel = 0;
+
+	SoundMan::ErrorCheck( m_pFmodSystem->playSound( FMOD_CHANNEL_FREE, m_pFmodSound, true, &m_pFmodChannel ) );
+	SoundMan::ErrorCheck( m_pFmodChannel->setLoopCount( in_bLoop ? -1 : 0 ) );
+	SoundMan::ErrorCheck( m_pFmodChannel->setPaused( false ) );		// This is where the sound really starts.
 }
 
 void Sound::Stop()
@@ -42,6 +47,10 @@ bool Sound::IsPaused()
 	return bPaused;
 }
 
+// ============================================================================
+
+#define SOUNDMAN_MAX_CHANNEL	128
+
 SoundMan::SoundMan()
 {
 	unsigned int      nVersion;
@@ -55,7 +64,7 @@ SoundMan::SoundMan()
 		return;
 	}
 
-	SoundMan::ErrorCheck( m_pFmodSystem->init(1, FMOD_INIT_NORMAL, 0) );
+	SoundMan::ErrorCheck( m_pFmodSystem->init(SOUNDMAN_MAX_CHANNEL, FMOD_INIT_NORMAL, 0) );
 }
 
 SoundMan::~SoundMan()
@@ -66,6 +75,32 @@ SoundMan::~SoundMan()
 	m_pFmodSystem = 0;
 }
 
+Sound* SoundMan::Get( const std::string& in_szFilename )
+{
+	std::map< std::string, Sound* >::iterator it = m_mapSound.find( in_szFilename );
+
+	if( it != m_mapSound.end() )
+	{
+		// found it, return it
+		Lair::GetLogMan()->Log( "SoundMan", "Loaded sound from map (%s)", in_szFilename.c_str() );
+		return it->second;
+	}
+	else
+	{
+		// not found, load it, return it
+		Sound* pSound = new Sound;
+		if( pSound->Load( in_szFilename, m_pFmodSystem ) )
+		{
+			m_mapSound.insert( std::make_pair(in_szFilename,pSound) );
+			Lair::GetLogMan()->Log( "SoundMan", "Loaded sound from file named %s", in_szFilename.c_str() );
+			return pSound;
+		}
+		Lair::GetLogMan()->Log( "SoundMan", "Could not load sound from file named %s", in_szFilename.c_str() );
+
+		delete pSound;
+	}
+	return 0;
+}
 
 void SoundMan::ErrorCheck( FMOD_RESULT in_FmodResult )
 {
