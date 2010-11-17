8f0f4bb3c7233f9c2581393db3a9f949e9e96d69
jimmy.beaubien <jimmy.beaubien@8ee7db41-403f-03ee-0b30-02c333d15193>
2010-11-17 05:21:08 +0000

    Added debug text support.

--- src/DebugDraw/DebugDraw.cpp
+++ src/DebugDraw/DebugDraw.cpp
@@ -3,6 +3,12 @@
 
 #include <glee/GLee.h>
 
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "../Lair/Lair.h"
+
 #define MAX_WORK_VECTOR_COUNT 32
 
 static Vector2 g_vTrigo[32] = 
@@ -49,10 +55,13 @@ CDebugDraw::CDebugDraw( unsigned int in_nCountLineVB, unsigned int in_nCountFill
 	m_pCurrVertex = m_pLineVB = new Vertex[m_nCountLineVB];
 	m_cCurrentColor = Color::eWHITE;
 	m_fCurrDepth = 0.0f;
+
+	m_pFont = new CDebugFont( "Font.png" );
 }
 
 CDebugDraw::~CDebugDraw()
 {
+	delete m_pFont;
 	delete[] m_pLineVB;
 	delete[] m_pWorkVector;
 }
@@ -205,4 +214,104 @@ void CDebugDraw::DrawPoly(const Vector2& pos, Vector2 poly[], unsigned int count
 		m_pWorkVector[i] = poly[i].GetRotate( angle ) + pos; 
 	}	
 	AddContourLinesFromPoints( m_pWorkVector, k );
+}
+
+
+void CDebugDraw::Text( int x, int y, char* text, ... )
+{
+	static char buffer[1024];
+	
+	va_list marker;
+	va_start( marker, text );
+	vsprintf( buffer, text, marker );
+	va_end(marker);
+
+	m_pFont->Text( Vector2(x,y), buffer );
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+void CDebugFont::Text( Vector2 in_vPos, Vector2 in_vScale, float in_cTextColor[4], const char* in_szText )
+{
+	glPushAttrib( GL_ENABLE_BIT );
+
+	glEnable( GL_TEXTURE_2D );
+	m_pFontTexture->Bind();
+
+	glEnable( GL_BLEND );
+	glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+
+	glColor4fv( in_cTextColor );
+
+	glPushMatrix();
+	glTranslatef( in_vPos.x, in_vPos.y, 0.0f );
+	glScalef( in_vScale.x, in_vScale.y, 1.0f );
+	glListBase(m_nFontListId);
+	glCallLists( strlen(in_szText), GL_UNSIGNED_BYTE, in_szText );
+	glListBase(0);
+	glPopMatrix();
+
+	glColor4f(1.0f,1.0f,1.0f,1.0f);
+
+	glPopAttrib();
+}
+
+CDebugFont::~CDebugFont()
+{
+	if( m_pFontTexture )
+	{
+		SAFE_DELETE( m_pFontTexture );
+	}
+
+	if( m_nFontListId )
+		glDeleteLists( m_nFontListId, 256 );
+}
+
+void CDebugFont::BuildFont( const char* in_szFontTextureName )
+{
+	if( m_nFontListId )
+		glDeleteLists( m_nFontListId, 256 );
+
+	if( in_szFontTextureName )
+		m_pFontTexture = Lair::GetTextureMan()->Get( in_szFontTextureName );
+
+	m_nFontListId = glGenLists(256);
+
+#define FONT_CHAR_PER_ROW	16
+#define FONT_CHAR_WIDTH 16
+#define FONT_CHAR_HEIGHT 16
+
+	m_nGlyphWidth = FONT_CHAR_WIDTH;		// fixme
+	m_nGlyphHeight = FONT_CHAR_HEIGHT;		// fixme
+
+	Vector2 uvMin, uvMax;
+
+	int i=0;
+	for( i=0; i<256; i++)
+	{
+		uvMax.x = (float)((uvMin.x = (float)((i % FONT_CHAR_PER_ROW) * FONT_CHAR_WIDTH)) + FONT_CHAR_WIDTH);
+		uvMin.y = (float)((uvMax.y = (float)((16 - (i / FONT_CHAR_PER_ROW)) * FONT_CHAR_HEIGHT)) - FONT_CHAR_HEIGHT);
+
+		uvMax.x /= 256.0f;
+		uvMax.y /= 256.0f;
+		uvMin.x /= 256.0f;
+		uvMin.y /= 256.0f;
+
+		//		uvMax.x = 1.0f;
+		//		uvMax.y = 1.0f;
+		//		uvMin.x = 0.0f;
+		//		uvMin.y = 0.0f;
+
+		glNewList( m_nFontListId + i, GL_COMPILE );
+
+		glBegin( GL_TRIANGLE_STRIP );
+		glTexCoord2f( uvMax.x, uvMin.y ); glVertex3f( 16.0f, 16.0f, 0.0f  ); 
+		glTexCoord2f( uvMax.x, uvMax.y ); glVertex3f( 16.0f,  0.0f, 0.0f  ); 
+		glTexCoord2f( uvMin.x, uvMin.y ); glVertex3f(  0.0f, 16.0f, 0.0f  ); 
+		glTexCoord2f( uvMin.x, uvMax.y ); glVertex3f(  0.0f,  0.0f, 0.0f  ); 
+		glEnd();
+
+		glTranslatef( FONT_CHAR_WIDTH, 0.0f, 0.0f );
+		glEndList();
+	}
 }
