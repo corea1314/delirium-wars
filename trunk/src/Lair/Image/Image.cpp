9b378f3ecdbab57cd3b1324ec6a8c137cfcc43a8
jimmy.beaubien <jimmy.beaubien@8ee7db41-403f-03ee-0b30-02c333d15193>
2011-01-04 05:09:45 +0000

    Modified interface a little to support buffer Image. Added Blit support.

--- trunk/src/Lair/Image/Image.cpp
+++ trunk/src/Lair/Image/Image.cpp
@@ -20,3 +20,48 @@ Image* ImageMan::Get( const std::string& in_szFilename )
 	
 	return new ImageRaw; // return a dummy image if it fails
 }
+
+bool Image::Clip( int& dx, int& dy, int dw, int dh, int& sx, int& sy, int& sw, int& sh )
+{
+	assert( dx >= 0 && dy >= 0 && "Image::Clip - Parameters out of range." );
+	assert( sx >= 0 && sy >= 0 && "Image::Clip - Parameters out of range." );
+
+	// assumed sw and sh are <= than width and height of image
+
+	sx = MAX( 0, sx );		// min: prevent reading outside of src
+	sy = MAX( 0, sy );
+
+	dx = MAX( 0, dx );		// min: prevent writing outside of dst
+	dy = MAX( 0, dy );
+
+	sw = MIN( sw, dw-dx );	// max: prevent writing outside of dst
+	sh = MIN( sh, dh-dy );
+
+	if( sw <= 0 || sh <= 0 )
+		return false; // returns null blit rect params
+
+	return true; // returns valid blit rect params
+}
+
+bool Image::Blit(	Image* in_pDst, int in_nDstX, int in_nDstY, 
+	Image* in_pSrc, int in_nSrcX, int in_nSrcY, int in_nSrcW, int in_nSrcH )
+{
+	if( !Clip( in_nDstX, in_nDstY, in_pDst->GetWidth(), in_pDst->GetHeight(), in_nSrcX, in_nSrcY, in_nSrcW, in_nSrcH ) )
+		return false;
+
+	const int nSrcP = in_pSrc->GetWidth()*in_pSrc->GetBytesPerPixel();
+	const int nDstP = in_pDst->GetWidth()*in_pDst->GetBytesPerPixel();
+
+	unsigned char* pDataSrc = in_pSrc->GetPixelBuffer() + (in_nSrcY * nSrcP) + in_nSrcX * in_pSrc->GetBytesPerPixel();
+	unsigned char* pDataDst = in_pDst->GetPixelBuffer() + (in_nDstY * nDstP) + in_nDstX * in_pDst->GetBytesPerPixel();
+
+	while(in_nSrcH--)
+	{
+		memcpy(pDataDst, pDataSrc, in_nSrcW * in_pSrc->GetBytesPerPixel() );
+
+		pDataSrc += nSrcP;
+		pDataDst += nDstP;
+	}
+
+	return true;
+}
