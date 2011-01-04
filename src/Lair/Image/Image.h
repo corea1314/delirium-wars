a33b11c78b53db591883e4d02cc5b0fc35034ed3
jimmy.beaubien <jimmy.beaubien@8ee7db41-403f-03ee-0b30-02c333d15193>
2011-01-04 05:09:45 +0000

    Modified interface a little to support buffer Image. Added Blit support.

--- src/Lair/Image/Image.h
+++ src/Lair/Image/Image.h
@@ -3,14 +3,23 @@
 
 #include <string>
 
+//todo: make this class clean. have image man use the proper loader. should never derived from image.
+
 class Image
 {
 public:
-	virtual bool Load( const std::string& in_szFilename ) = 0;
-	virtual unsigned long GetWidth() const = 0;
-	virtual unsigned long GetHeight() const = 0;
-	virtual unsigned long GetBytePerPixel()  = 0;
-	virtual unsigned char* GetPixelBuffer() = 0;
+	Image() {}
+	Image( unsigned long in_nWidth, unsigned long in_nHeight, unsigned long in_nBytePerPixel ) {}
+	
+	virtual bool Load( const std::string& in_szFilename );
+	virtual unsigned long GetWidth() const;
+	virtual unsigned long GetHeight() const;
+	virtual unsigned long GetBytesPerPixel();
+	virtual unsigned char* GetPixelBuffer();
+
+	static bool Clip( int& dx, int& dy, int dw, int dh, int& sx, int& sy, int& sw, int& sh );
+	static bool Blit( Image* in_pDst, int in_nDstX, int in_nDstY, 
+					  Image* in_pSrc, int in_nSrcX, int in_nSrcY, int in_SrcW, int in_nSrcH );
 };
 
 class ImageMan
