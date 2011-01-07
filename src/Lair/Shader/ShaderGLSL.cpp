f5fbdd58c041102a6d2214f79df1c10f53053044
jimmy.beaubien <jimmy.beaubien@8ee7db41-403f-03ee-0b30-02c333d15193>
2011-01-07 11:07:47 +0000

    Added geometry shader support. Shader creation now more robust.

--- src/Lair/Shader/ShaderGLSL.cpp
+++ src/Lair/Shader/ShaderGLSL.cpp
@@ -8,12 +8,10 @@
 
 #include <stdio.h>
 
-const char*	ShaderGLSL::ms_szLoadFilename = 0;
-
 char* GetBufferFromFile( const char* in_szFilename )
 {
 	FILE* pFile;
-	if( fopen_s( &pFile, in_szFilename, "rt" ) == 0 )
+	if( fopen_s( &pFile, in_szFilename, "rb" ) == 0 )
 	{
 		fseek( pFile, 0, SEEK_END );
 		unsigned int nSize = ftell( pFile );
@@ -36,6 +34,7 @@ ShaderGLSL::ShaderGLSL()
 {
 	m_iVertShader = 0;
 	m_iFragShader = 0;
+	m_iGeomShader = 0;
 	m_iProgram = 0;
 	m_iUniformSampler[0] = m_iUniformSampler[1] = m_iUniformSampler[2] = m_iUniformSampler[3] = 0;
 
@@ -67,69 +66,69 @@ void ShaderGLSL::Unbind()
 	glUseProgram(0);
 }
 
-bool ShaderGLSL::Create( const char* in_szFilename ) 
+bool ShaderGLSL::CreateShader( unsigned int & in_iShader, unsigned int in_eShaderType, const char* in_szFilename )
+{
+	char *s = 0;
+	if( (s = GetBufferFromFile(in_szFilename)) == 0 )
+	{
+		Lair::GetLogMan()->Log( "ShaderGLSL", "ShaderGLSL - Could not load shader named \"%s\"", in_szFilename );
+		SAFE_DELETE_ARRAY(s);
+		return false;
+	}
+
+	in_iShader = glCreateShader(in_eShaderType);
+	glShaderSourceARB(in_iShader, 1, (const char**)&s, NULL);
+	glCompileShader(in_iShader);
+
+	if( ValidateShader(in_iShader,in_szFilename) == false )
+	{
+		SAFE_DELETE_ARRAY(s);
+		return false;
+	}
+	SAFE_DELETE_ARRAY(s);
+	return true;
+}
+
+bool ShaderGLSL::Create( const char* in_szFilename, unsigned int in_eInPrimType, unsigned int in_eOutPrimType, unsigned int in_nMaxOutVertices ) 
 { 	
-	char szFilenameVS[MAX_PATH], szFilenameFS[MAX_PATH];
-	
-	#define _GLSL_CLEANUP	\
-		SAFE_DELETE_ARRAY(vs);						\
-		SAFE_DELETE_ARRAY(fs);						\
-		if( m_iProgram && m_iVertShader )	glDetachShader(m_iProgram,m_iVertShader);	\
-		if( m_iProgram && m_iFragShader )	glDetachShader(m_iProgram,m_iFragShader);	\
-		if( m_iVertShader )	glDeleteShader(m_iVertShader);	\
-		if( m_iFragShader )	glDeleteShader(m_iFragShader);	\
-		if( m_iProgram )	glDeleteProgram(m_iProgram);
-
-	char *vs = 0,*fs = 0;
-
-	sprintf_s( szFilenameVS, "%s.vs", in_szFilename );
-	if( vs = GetBufferFromFile(szFilenameVS) )
-	{
-		Lair::GetLogMan()->Log( "ShaderGLSL", "ShaderGLSL - Could not load vertex shader named \"%s\"", szFilenameVS );
-		_GLSL_CLEANUP;
-		return false;
-	}
+	char szFilename[MAX_PATH];
 		
-	sprintf_s( szFilenameFS, "%s.fs", in_szFilename );
-	if( fs = GetBufferFromFile(szFilenameFS) )
+	sprintf_s( szFilename, "%s.vs", in_szFilename );
+	if( !CreateShader( m_iVertShader, GL_VERTEX_SHADER, szFilename ) )
 	{
-		Lair::GetLogMan()->Log( "ShaderGLSL", "ShaderGLSL - Could not load fragment shader named \"%s\"", szFilenameFS );
-		_GLSL_CLEANUP;
+		Destroy();
 		return false;
 	}
 	
-	m_iVertShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSourceARB(m_iVertShader, 1, (const char**)&vs, NULL);
-	glCompileShader(m_iVertShader);
-	
-	m_iFragShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSourceARB(m_iFragShader, 1, (const char**)&fs, NULL);
-	glCompileShader(m_iFragShader);
-
-	SAFE_DELETE_ARRAY(fs);
-	SAFE_DELETE_ARRAY(vs);
-
-	if( ValidateShader(m_iVertShader,szFilenameVS) == false || 
-		ValidateShader(m_iFragShader,szFilenameFS) == false )
+	sprintf_s( szFilename, "%s.fs", in_szFilename );
+	if( !CreateShader( m_iFragShader, GL_FRAGMENT_SHADER, szFilename ) )
 	{
-		_GLSL_CLEANUP;
+		Destroy();
 		return false;
-	}
-
+	}	
+	
+	sprintf_s( szFilename, "%s.gs", in_szFilename );
+	CreateShader( m_iGeomShader, GL_GEOMETRY_SHADER_EXT, szFilename );	//might not be present
+	
 	m_iProgram = glCreateProgram();
 	
 	glAttachShader(m_iProgram,m_iVertShader);
 	glAttachShader(m_iProgram,m_iFragShader);
 
+	if( m_iGeomShader )
+	{
+		glAttachShader(m_iProgram,m_iGeomShader);
+		glProgramParameteriEXT(m_iProgram,GL_GEOMETRY_INPUT_TYPE_EXT,in_eInPrimType);
+		glProgramParameteriEXT(m_iProgram,GL_GEOMETRY_OUTPUT_TYPE_EXT,in_eOutPrimType);
+		glProgramParameteriEXT(m_iProgram,GL_GEOMETRY_VERTICES_OUT_EXT, in_nMaxOutVertices );
+	}
+	
 	glLinkProgram(m_iProgram);
 	glValidateProgram(m_iProgram);
 
-	vs = 0;
-	fs = 0;
-	
-	if( ValidateProgram(m_iProgram) == false )
+	if( ValidateProgram(m_iProgram, in_szFilename ) == false )
 	{
-		_GLSL_CLEANUP;
+		Destroy();
 		return false;
 	}
 
@@ -146,18 +145,13 @@ bool ShaderGLSL::Create( const char* in_szFilename )
 
 void ShaderGLSL::Destroy()
 {
-	if( m_iProgram && m_iVertShader )
-		glDetachShader(m_iProgram,m_iVertShader);
-	if( m_iProgram && m_iFragShader )
-		glDetachShader(m_iProgram,m_iFragShader);
-
-	if( m_iVertShader )
-		glDeleteShader(m_iVertShader);
-	if( m_iFragShader )
-		glDeleteShader(m_iFragShader);
-	
-	if( m_iProgram )
-		glDeleteProgram(m_iProgram);
+	if( m_iProgram && m_iVertShader )	glDetachShader(m_iProgram,m_iVertShader);
+	if( m_iProgram && m_iFragShader )	glDetachShader(m_iProgram,m_iFragShader);
+	if( m_iProgram && m_iGeomShader )	glDetachShader(m_iProgram,m_iGeomShader);
+	if( m_iVertShader )	{ glDeleteShader(m_iVertShader); m_iVertShader = 0;	}
+	if( m_iFragShader )	{ glDeleteShader(m_iFragShader); m_iFragShader = 0;	}
+	if( m_iGeomShader )	{ glDeleteShader(m_iGeomShader); m_iGeomShader = 0;	}
+	if( m_iProgram )	{ glDeleteProgram(m_iProgram); m_iProgram = 0; }
 }
 
 bool ShaderGLSL::ValidateShader( unsigned int in_iShader, const char* in_szShaderName )
@@ -177,7 +171,7 @@ bool ShaderGLSL::ValidateShader( unsigned int in_iShader, const char* in_szShade
 		{
 			szInfoLog = new char[nInfologLength];
 			glGetShaderInfoLog( in_iShader, nInfologLength, &nCharsWritten, szInfoLog );
-			Lair::GetLogMan()->Log( "ShaderGLSL", "ValidateShader(%s, %s) - Error(s) detected:\n%s", ms_szLoadFilename, in_szShaderName, szInfoLog );
+			Lair::GetLogMan()->Log( "ShaderGLSL", "ValidateShader(%s) - Error(s) detected:\n%s", in_szShaderName, szInfoLog );
 			delete[] szInfoLog;
 			return false;
 		}
@@ -185,7 +179,7 @@ bool ShaderGLSL::ValidateShader( unsigned int in_iShader, const char* in_szShade
 	return true;
 }
 
-bool ShaderGLSL::ValidateProgram(unsigned int in_iProgram)
+bool ShaderGLSL::ValidateProgram(unsigned int in_iProgram, const char* in_szFilename )
 {
 	int nInfologLength = 0;
 	int nCharsWritten  = 0;
@@ -203,7 +197,7 @@ bool ShaderGLSL::ValidateProgram(unsigned int in_iProgram)
 		{
 			szInfoLog = new char[nInfologLength];
 			glGetProgramInfoLog( in_iProgram, nInfologLength, &nCharsWritten, szInfoLog );
-			Lair::GetLogMan()->Log( "ShaderGLSL", "ValidateProgram(%s) - Error(s) detected:\n%", ms_szLoadFilename, szInfoLog);
+			Lair::GetLogMan()->Log( "ShaderGLSL", "ValidateProgram(%s) - Error(s) detected:\n%", in_szFilename, szInfoLog);
 			delete[] szInfoLog;
 			return false;
 		}
--- src/Lair/Shader/ShaderGLSL.h
+++ src/Lair/Shader/ShaderGLSL.h
@@ -1,8 +1,7 @@
 #ifndef _SHADERGLSL_H
 #define _SHADERGLSL_H
 
-
-class TiXmlElement;
+#include "glee/GLee.h"
 
 class ShaderGLSL
 {
@@ -13,26 +12,25 @@ public:
 	void Bind();
 	void Unbind();
 	
-	bool Create( const char* in_szFilename );
+	bool Create( const char* in_szFilename, unsigned int in_eInPrimType = GL_TRIANGLES, unsigned int in_eOutPrimType = GL_TRIANGLE_STRIP, unsigned int in_nMaxOutVertices = 8 ) ;
 	void Destroy();
 
 protected:
-	bool LoadFromXML( TiXmlElement* in_pxmlProgram );
+	bool CreateShader( unsigned int & in_iShader, unsigned int in_eShaderType, const char* in_szFilename );
 
-	bool ValidateShader( unsigned int, const char* );
-	bool ValidateProgram( unsigned int );
+	bool ValidateShader( unsigned int in_iShader, const char* in_szFilename);
+	bool ValidateProgram(unsigned int in_iProgram, const char* in_szFilename );
 
 private:
 	unsigned int	m_iVertShader;
 	unsigned int	m_iFragShader;
+	unsigned int	m_iGeomShader;
 	unsigned int	m_iProgram;
 
 	unsigned int m_iUniformSampler[4];
 
 	unsigned int	m_iUniformTotalTime;
 	unsigned int	m_iUniformDeltaTime;
-
-	static const char*	ms_szLoadFilename;
 };
 
 #endif//_SHADERGLSL_H
