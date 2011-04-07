97b57becd090d10bb8b1f5c25cb0e3971defc418
jimmy.beaubien <jimmy.beaubien@8ee7db41-403f-03ee-0b30-02c333d15193>
2011-04-07 04:47:49 +0000

    Added AtlasMan

--- trunk/src/Lair/Atlas/Atlas.h
+++ trunk/src/Lair/Atlas/Atlas.h
@@ -1,71 +1,42 @@
 #ifndef _ATLAS_H
 #define _ATLAS_H
 
-#include "Packing/GuillotineBinPack.h"
+class Atlas;
+class AtlasPack;
 
-typedef GuillotineBinPack Bin;	// using guillotine
-
-class Image;
-class Texture;
+#include <string>
 
 #include "../../Math/Vector2.h"
 
-#include <map>
-#include <vector>
-
-class Atlas
+class AtlasIndex
 {
-private:
-	class Pack
-	{
-	public:
-		Pack( unsigned long w, unsigned long h );
-		virtual ~Pack();
-
-		void ReloadTexture();
-
-		Bin*		bin;
-		Image*		img;
-		Texture*	tex;	// fixme, find a better place
-	};
-
 public:
-	class Index
-	{
-	public:
-		const Vector2& GetSize() const { return size;	}
-		const Vector2& GetOffset() const { return offset; }
-		const Vector2& GetMinUV() const { return uv_min; }
-		const Vector2& GetMaxUV() const { return uv_max; }
-
-	private:
-		Vector2 size;			// world space size (width,height)
-		Vector2 offset;			// world space offset about pos
-		Vector2 uv_min, uv_max;	// texture coordinates
-		Pack*			pack;
-
-		friend class Atlas;
-	};
+	const Vector2& GetSize() const { return size;	}
+	const Vector2& GetOffset() const { return offset; }
+	const Vector2& GetMinUV() const { return uv_min; }
+	const Vector2& GetMaxUV() const { return uv_max; }
 
 private:
-	const unsigned long m_nBinWidth;	// maximal width of the bins
-	const unsigned long m_nBinHeight;	// maximal height of the bins
+	Vector2 size;			// world space size (width,height)
+	Vector2 offset;			// world space offset about pos
+	Vector2 uv_min, uv_max;	// texture coordinates
+	AtlasPack*	pack;
 
-	std::vector<Pack*>	m_vecPacks;		// vector of all the allocated bins
-	std::vector<Pack*>::iterator	m_itCurrPack;
-	std::map<std::string,Index*>	m_mapIndices;	// map of all the individual index (leading to its bin, img and coordinates)
+	friend class Atlas;
+};
 
-private:
-	Index*	InsertFromFile( const std::string& in_szFilename );
-	Index*	AddImageToPack( Pack* in_pPack, const Rect& in_rectCoords, Image* in_pImage, int in_nOffsetX, int in_nOffsetY );
-
+class AtlasMan
+{
 public:
-	Atlas( const unsigned long in_nBinWidth, const unsigned long in_nBinHeight ) ;
+	AtlasMan();
+	virtual ~AtlasMan();
+	AtlasIndex* Get( const std::string& in_szFilename );
 
-	Index*	Get( const std::string& in_szFilename );
+	void Bind();	//fixme
+	void Reload();
 
-	void BindTexture( unsigned int in_nPackIndex );
-	void ReloadTexture();
+private:
+	Atlas*	m_pAtlas;
 };
 
 #endif//_ATLAS_H
